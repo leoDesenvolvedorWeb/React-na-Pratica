4a94ae92688fb220dc97c2d23b81f6da67564409
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2025-03-10 18:45:57 -0300

    Limpando o react para trabalhar

---
+++ todo/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+
+
+  ])
+
+  return (
+    <div className="app">
+      
+    </div>
+  )
+}
+
+export default App
