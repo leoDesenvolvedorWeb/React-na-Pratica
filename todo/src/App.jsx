860f88526d70afb3049ca547bad70d4f7dc69d7c
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2025-03-11 15:01:40 -0300

    Feat: adicionando o todo liste

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import "./App.css";
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -26,7 +28,21 @@ function App() {
 
   return (
     <div className="app">
-      
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+              </div>
+              <div>
+                <button>Completar</button>
+                <button>x</button>
+              </div>
+            </div>
+        ))}
+      </div>
     </div>
   )
 }
